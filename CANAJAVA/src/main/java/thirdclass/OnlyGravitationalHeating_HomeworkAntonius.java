94d4c2a3c0e3cbf7b6f28e5bfb1980f54c04a4a0
Antonius Frie <antonius.frie@rub.de>
2017-10-25 12:28:13 +0200

    Move OnlyGravitationalHeating back into the old package,
    change graph appearance, save graph

--- CANAJAVA/src/main/java/thirdclass/OnlyGravitationalHeating_HomeworkAntonius.java
+++ CANAJAVA/src/main/java/thirdclass/OnlyGravitationalHeating_HomeworkAntonius.java
@@ -1,30 +1,36 @@
-package homework_antonius;
+package thirdclass;
 
 import org.jlab.groot.data.GraphErrors;
 import org.jlab.groot.ui.TCanvas;
 import org.jline.terminal.Size;
 
 import domain.utils.Constants;
+import domain.utils.SaveCanvas;
 
-public class OnlyGravitationalHeating {
+public class OnlyGravitationalHeating_HomeworkAntonius {
 
 	public static void main(String[] args) {
-		OnlyGravitationalHeating onlyGravitationalHeating = new OnlyGravitationalHeating();
+		OnlyGravitationalHeating_HomeworkAntonius onlyGravitationalHeating = new OnlyGravitationalHeating_HomeworkAntonius();
 		
 		GraphErrors gr = new GraphErrors();
 
 		
 		for (int i = 1; i <= 20; i++) {
 			System.out.println(onlyGravitationalHeating.radius(i));
-			gr.addPoint((double) i, onlyGravitationalHeating.radius(i) / Constants.solarRadius, 0., 0.);
+			gr.addPoint(i, onlyGravitationalHeating.radius(i) / Constants.solarRadius, 0., 0.);
 		}
 		
 		gr.setTitle("Theoretical solar radius if the sun relied only on gravitational heating.");
 		gr.setTitleX("age / age of sun");
 		gr.setTitleY("r / r_sun");
+		gr.setMarkerColor(5);
+		gr.setMarkerStyle(1);
 		
 		TCanvas canvas = new TCanvas("plot", 640, 400);
 		canvas.draw(gr);
+		SaveCanvas.saveCanvas(canvas);
+		
+		canvas.dispose();
 	}
 	
 	public double radius() {
